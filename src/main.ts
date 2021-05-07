426e8ae77c768926094bb6b99d3c49c562da0a0c
Joshua <joshua.stuebner@bloxxter.com>
2021-05-07 16:23:18 +0200

    feat(demo): more content

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,5 @@ import './assets/styles/style.css';
 
 import './components/ThemeSwitcher/ThemeSwitcher';
 import './components/Modal/Modal';
+import './components/Accordion/Accordion';
+import './components/Icon/Icon';
