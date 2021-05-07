66755823c29d44bf89f73707b4e8646ec8b80bf5
Joshua <joshua.stuebner@bloxxter.com>
2021-05-07 16:23:08 +0200

    feat(modal): add more accessibilty and add theme support

--- src/components/Modal/Modal.ts
+++ src/components/Modal/Modal.ts
@@ -8,37 +8,44 @@ export class BlxModalElement extends HTMLElement {
   @target overlay!: HTMLElement;
   @target content!: HTMLElement;
 
+  @target close!: HTMLElement | null;
+  @target modalTitle!: HTMLElement | null;
+
   @attr isOpen: boolean = false;
 
+  private previouslyActiveElement: HTMLElement | null = null;
   private subscriptions: Subscription[] = [];
 
   connectedCallback() {
     this.container.setAttribute('role', 'dialog');
     this.container.setAttribute('aria-modal', 'true');
+
+    if (this.modalTitle) {
+      const modalTitleId = `modal-title-id-${this.modalTitle.textContent}`;
+      this.modalTitle.setAttribute('id', modalTitleId);
+      this.container.setAttribute('aria-labeledby', modalTitleId);
+    }
   }
 
   attributeChangedCallback() {
     if (this.isOpen) {
-      this.container.classList.add('open');
-      this.overlay.classList.add('fade-in-overlay');
-      this.content.classList.add('fade-in-content');
+      this.addOpeningClasses();
+
+      this.previouslyActiveElement = document.activeElement as HTMLElement;
+
+      if (this.close) {
+        this.close.focus();
+      }
 
       this.setupSubscriptions();
     }
     if (!this.isOpen) {
-      this.overlay.classList.remove('fade-in-overlay');
-      this.content.classList.remove('fade-in-content');
-      this.overlay.classList.add('fade-out-overlay');
-      this.content.classList.add('fade-out-content');
+      this.handleClosingClasses();
+
+      this.previouslyActiveElement?.focus();
+      this.previouslyActiveElement = null;
 
       this.cleanupSubscriptions();
-
-      setTimeout(() => {
-        this.container.classList.remove('open');
-        this.overlay.classList.remove('fade-out-overlay');
-        this.content.classList.remove('fade-out-content');
-        this;
-      }, 300);
     }
   }
 
@@ -48,7 +55,7 @@ export class BlxModalElement extends HTMLElement {
 
   private setupSubscriptions = () => {
     this.subscriptions = [
-      fromEvent(this.overlay, 'click', () => this.close()),
+      fromEvent(this.overlay, 'click', () => this.dismiss()),
       fromEvent(this, 'keydown', (e) => this.handleKeydownEvents(e as KeyboardEvent)),
     ];
   };
@@ -59,15 +66,35 @@ export class BlxModalElement extends HTMLElement {
 
   private handleKeydownEvents = (event: KeyboardEvent) => {
     if (event.key === 'Escape') {
-      this.close();
+      this.dismiss();
     }
+  };
+
+  private addOpeningClasses = () => {
+    this.container.classList.add('open');
+    this.overlay.classList.add('fade-in-overlay');
+    this.content.classList.add('fade-in-content');
+  };
+
+  private handleClosingClasses = () => {
+    this.overlay.classList.remove('fade-in-overlay');
+    this.content.classList.remove('fade-in-content');
+    this.overlay.classList.add('fade-out-overlay');
+    this.content.classList.add('fade-out-content');
+
+    setTimeout(() => {
+      this.container.classList.remove('open');
+      this.overlay.classList.remove('fade-out-overlay');
+      this.content.classList.remove('fade-out-content');
+      this;
+    }, 300);
   };
 
   public open() {
     this.isOpen = true;
   }
 
-  public close() {
+  public dismiss() {
     this.isOpen = false;
   }
 
