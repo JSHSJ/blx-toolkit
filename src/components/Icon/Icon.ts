716fca3915a7f189c4c033904004132589652ad6
Joshua <joshua.stuebner@bloxxter.com>
2021-05-18 13:26:42 +0200

    feat(*): small tweaks

--- src/components/Icon/Icon.ts
+++ src/components/Icon/Icon.ts
@@ -11,33 +11,41 @@ export class BlxIconElement extends HTMLElement {
   getSize() {
     switch (this.size) {
       case 'xs':
-        return { width: '0.5em', height: '0.5em' };
+        return { width: '1rem', height: '1rem' };
       case 's':
-        return { width: '0.75em', height: '0.75em' };
+        return { width: '1.5rem', height: '1.5rem' };
       case 'm':
-        return { width: '1em', height: '1em' };
+        return { width: '2rem', height: '2rem' };
       case 'l':
-        return { width: '1.25em', height: '1.25em' };
+        return { width: '2.5rem', height: '2.5rem' };
       case 'xl':
-        return { width: '1.5em', height: '1.5em' };
+        return { width: '3rem', height: '3rem' };
       case '2xl':
-        return { width: '1.75em', height: '1.75em' };
+        return { width: '4rem', height: '4rem' };
+      case 'inline':
+        return { width: '1.25em', height: '1.25em' };
       case 'custom':
         return { width: this.width, height: this.height };
       default:
-        return { width: '1em', height: '1em' };
+        return { width: '2rem', height: '2rem' };
     }
   }
 
   connectedCallback() {
+    this.updateTemplate();
+  }
+
+  disconnectedCallback() {}
+
+  attributeChangedCallback() {
+    this.updateTemplate();
+  }
+
+  private updateTemplate = () => {
     this.innerHTML = `
     <svg viewBox="0 0 32 32" width="${this.getSize().width}" height="${this.getSize().height}">
       <use xlink:href="icons/${this.name}.svg/#${this.name}"></use>
     </svg>
     `;
-  }
-
-  disconnectedCallback() {}
-
-  attributeChangedCallback() {}
+  };
 }
